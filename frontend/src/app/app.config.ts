f2633b39d1bb78197c75dd89fbef52dc8cfe09fd
JENŐ <90604013+Kiss-Martin@users.noreply.github.com>
2026-02-11 11:24:12 +0100

    ikonok hozzáadása a projekthez

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { LucideAngularModule, Monitor } from 'lucide-angular';
+import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(LucideAngularModule.pick({ Monitor })),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
